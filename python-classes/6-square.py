e69e17ea55af921f583a1040bad5feca50ea6586
root <t@e5ae246a1093one)>
2025-07-01 08:59:57 -0700

    #

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
+
 """Defines a class Square with size and position attributes."""
+
 
 class Square:
     """Represents a square with size and position."""
@@ -16,8 +19,8 @@ class Square:
             TypeError: if size or position are invalid
             ValueError: if size < 0
         """
-        self.size = size          # validated by setter
-        self.position = position  # validated by setter
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -59,9 +62,9 @@ class Square:
             TypeError: if value is not a tuple of 2 positive integers
         """
         if (not isinstance(value, tuple) or
-            len(value) != 2 or
-            not all(isinstance(num, int) for num in value) or
-            not all(num >= 0 for num in value)):
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
@@ -81,10 +84,8 @@ class Square:
             print()
             return
 
-        # Print new lines for vertical offset (position[1])
         for _ in range(self.__position[1]):
             print()
 
-        # Print each row with horizontal offset (position[0]) and '#' characters
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
